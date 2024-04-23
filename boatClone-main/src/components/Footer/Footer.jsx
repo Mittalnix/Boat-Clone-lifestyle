d3d13a6503c13fc1cb01789d5cd53505e6553d2f
mittalnix <nikhilmittal6377@gmail.com>
2024-04-23 19:44:25 +0530

    first commit

---
+++ boatClone-main/src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+import "./Footer.css";
+
+export function Footer() {
+  return (
+    <>
+      <div className="footer-main">
+        <div className="footer-links">
+          <a
+            href="https://www.linkedin.com/in/nikhil-mittal-179302197/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/Mittalnix" target="_blank">
+            <FaGithub />
+          </a>
+        </div>
+        <div className="footer-text">
+          Developed by Nikhil Mittal | © No Copyright, Feel free to replicate.
+        </div>
+      </div>
+    </>
+  );
+}
